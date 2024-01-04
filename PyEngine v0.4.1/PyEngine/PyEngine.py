d4278831decb810171e34cc3f036fdd6d0b8cd4f
Jaden909 <118631735+Jaden909@users.noreply.github.com>
2024-01-04 16:46:24 -0500

    update

--- PyEngine v0.4.1/PyEngine/PyEngine.py
+++ PyEngine v0.4.1/PyEngine/PyEngine.py
@@ -1,29 +1,11 @@
-"""Game Engine for Python. Requires Pygame."""
+"""Game Engine for Python. Requires Pygame.""" 
 import pygame,json,os,sys,importlib
+pygame.init()
 _mouseDown=False
 buttons=[]
 messages=[]
-def animation(moveList:list,fps:int,frames:int,screen,x,y):
-    """Pygame animation. (WIP)"""
-    for i in range(frames):    
-        if i>len(moveList):
-            i=0
-        clock=pygame.time.Clock()
-        clock.tick(fps)
-        currentSprite=moveList[i]
-        screen.blit(currentSprite,(x,y))
-        pygame.display.update()
-def wasdInput(WFunction=None,AFunction=None,SFunction=None,DFunction=None):
-    """Simple WASD/arrow keys input listener. Also supports arrow keys. Args are functions to run when respective key is pressed"""
-    keys=pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]or keys[pygame.K_a]and AFunction is not None:
-        AFunction()
-    if keys[pygame.K_RIGHT]or keys[pygame.K_d]and DFunction is not None:
-        DFunction()
-    if keys[pygame.K_DOWN]or keys[pygame.K_s]and SFunction is not None:
-        SFunction()
-    if keys[pygame.K_UP]or keys[pygame.K_w]and WFunction is not None:
-        WFunction()
+elements=[]
+
 class Message:
     "Creates a message that can be sent and recieved"
     def __init__(self,name):
@@ -31,18 +13,14 @@ class Message:
         message={'name':self.name,'state':False}
         messages.append(message)
     def send(self):
-        'Triggers all objects using this message'
         for message in messages:
             if message.get('name')==self.name:
                 message.update(state=True)
     def unsend(self):
-        'Stops triggering objects using this message'
         for message in messages:
             if message.get('name')==self.name:
                 message.update(state=False)
-            
-    def listen(self) -> bool: 
-        'Returns whether or not this message is active'
+    def listen(self):
         for message in messages:
             if message.get('state'):
                 return True
@@ -50,7 +28,7 @@ class Message:
                 return False
 class GameButton:
     """Create a button that can trigger a function if clicked on or a message is recieved (if one is provided)"""
-    def __init__(self,x:int=0,y:int=0,function=None,notHoverSprite:pygame.Surface='default',hover:bool=True,active:bool=True,hoverSprite:pygame.Surface='default',imageRes:int=64,image:str='Defaults\\DEFAULTBUTTON.png',imageResX:int=0,imageResY:int=0,message:Message=None):
+    def __init__(self,x:int=0,y:int=0,function=None,notHoverSprite:pygame.Surface='default',hover:bool=True,active:bool=True,hoverSprite:pygame.Surface='default',imageRes:int=64,image:str='Defaults\\DEFAULTBUTTON.png',imageResX:int=0,imageResY:int=0,message:Message=None,hoverAlt=None):
         self.hovering=False
         #REQUIRED ARGS  
         self.x=x
@@ -70,23 +48,30 @@ class GameButton:
         self.notHoverSprite=notHoverSprite
         if hoverSprite=='default':
             self.hoverCursor=pygame.SYSTEM_CURSOR_HAND
+        elif hoverSprite=='ibeam':
+            self.hoverCursor=pygame.SYSTEM_CURSOR_IBEAM
         else:
             self.hoverCursor=pygame.cursors.Cursor((0,0),self.hoverSprite)
         if notHoverSprite=='default':
-            self.notHoverCursor=pygame.SYSTEM_CURSOR_ARROW    
+            self.notHoverCursor=pygame.SYSTEM_CURSOR_ARROW   
+        elif notHoverSprite=='ibeam':
+            self.notHoverCursor=pygame.SYSTEM_CURSOR_IBEAM 
         else:
             self.notHoverCursor=pygame.cursors.Cursor((0,0),self.notHoverSprite) 
         self.active=active
         self.message=message
+        if hoverAlt is not None:
+            self.hoverAlt=pygame.image.load(hoverAlt)
+        else:
+            self.hoverAlt=None
         buttons.append(self)    
     #Blits button to screen provided
-    def show(self,screen):
+    def show(self,screen:pygame.surface.Surface):
         'Blit button to screen. Screen to blit to required. Requires defined image'
-        screenn=screen
-        screenn.blit(self.image,(self.x,self.y))
+        screen.blit(self.image,(self.x,self.y))
     
     #Listens for mouse clicks: should be in a loop to work properly
-    def listen(self):    
+    def listen(self,screen=None):    
         "Listens for clicks on the button. Should be in a loop."
         global _mouseDown
         if self.active:
@@ -97,6 +82,8 @@ class GameButton:
                     mouseX,mouseY=pygame.mouse.get_pos()
                     if mouseX>self.x and mouseX<self.x+self.imageRes and mouseY>self.y and mouseY<self.y+self.imageRes:
                         pygame.mouse.set_cursor(self.hoverCursor)
+                        if self.hoverAlt is not None and screen is not None:
+                            screen.blit(self.hoverAlt,(self.x,self.y))
                         self.hovering=True
                     elif self.hovering:
                         pygame.mouse.set_cursor(self.notHoverCursor)
@@ -105,10 +92,13 @@ class GameButton:
                     mouseX,mouseY=pygame.mouse.get_pos()
                     if mouseX>self.x and mouseX<self.x+self.imageResX and mouseY>self.y and mouseY<self.y+self.imageResY:
                         pygame.mouse.set_cursor(self.hoverCursor)
+                        if self.hoverAlt is not None and screen is not None:
+                            screen.blit(self.hoverAlt,(self.x,self.y))
                         self.hovering=True
                     elif self.hovering:
                         pygame.mouse.set_cursor(self.notHoverCursor)
                         self.hovering=False
+            
             #CLick Event
             if left and self.function:
                 if self.square:    
@@ -135,23 +125,15 @@ class GameButton:
             else:
                 mouseX,mouseY=pygame.mouse.get_pos()
                 if mouseX>self.x and mouseX<self.x+self.imageResX and mouseY>self.y and mouseY<self.y+self.imageResY:
-                    self.function()   
-def listenAll():
-    "Listen on all created buttons at once"
-    global buttons
-    for button in buttons:
-        button.listen()
-def listenPulseAll():
-    "Listen pulse on all created buttons at once"
-    global buttons
-    for button in buttons:
-        button.listenPulse()
+                    self.function() 
+    def enable(self):
+        self.active=True
+    def disable(self):
+        self.active=False
 class Vector2:
-    'Tuple of xy values (WIP)'
     def __init__(self,x:int|float,y:int|float):
         self.value=(x,y)
     def translate(self,x=0,y=0):
-        'Change x/y by specified amount. Negative numbers subtract.'
         self.x=self.value.__getitem__(0)
         self.y=self.value.__getitem__(1)
         self.x+=x
@@ -165,14 +147,112 @@ class staticImage:
         self.image=pygame.image.load(image).convert()
     def show(self,screen):
         screen.blit(self.image,(self.x,self.y))
+class TextBox:
+    def __init__(self,text:str,x:int,y:int,height:int,width:int,fontName:str,size:int,bold:bool,italic:bool,textColor:str|tuple,bgColor:str|tuple|None,borderColor:str|tuple|None,borderWidth:int,textOffset:tuple):
+        self.text=text
+        self.x=x
+        self.y=y
+        self.height=height
+        self.width=width
+        self.textColor=textColor
+        self.rect=pygame.rect.Rect(self.x,self.y,self.width,self.height)
+        self.mask=pygame.Mask((self.width,self.height),True)
+        self.bgColor=bgColor
+        self.borderColor=borderColor
+        self.borderWidth=borderWidth
+        self.textOffset=textOffset
+        self.font=pygame.font.Font(fontName,size)
+        #self.font=pygame.font.SysFont(fontName,size,bold,italic)
+        self.textBox=self.font.render(self.text,False,textColor)
+        if bgColor is not None:
+            self.bg=pygame.Surface((self.width,self.height))
+            self.bg.fill(self.bgColor)
+        else:
+            self.bg=None
+        elements.append(self)
+    def update(self,text:str):
+        'Update the text Displayed by the text box'
+        self.text=text
+        self.textBox=self.font.render(self.text,True,self.textColor)
+    def render(self,screen:pygame.Surface,autoFit=True):
+        if autoFit:
+            self.rect.width=self.font.size(self.text)[0]+self.textOffset[0]*2
+            self.width=self.font.size(self.text)[0]+self.textOffset[0]*2
+            if self.bg is not None:
+                self.bg=pygame.Surface((self.width,self.height))
+                self.bg.fill(self.bgColor)
+                screen.blit(self.bg,self.rect)
+        screen.blit(self.textBox,self.rect.move(self.textOffset))
+        if self.borderColor is not None:
+            pygame.draw.rect(screen,self.borderColor,self.rect,self.borderWidth)
+    def snapToMouse(self,borderX:int):
+        if self.rect.right<borderX:
+            self.rect.bottomleft=pygame.mouse.get_pos()
+        if self.rect.right>borderX:
+            self.rect.bottomright=pygame.mouse.get_pos()
 
-#Save/Load system
+        
+
+
+def animation(moveList:list,frameDelay:int,screen:pygame.Surface,x,y,startTime:int,frame:int):
+    """Pygame animation. returns current frame, startTime and done. Put this in a loop and supply the returned frame and startTime to work properly."""
+    framee=frame
+    currentTime = pygame.time.get_ticks()
+    done=False
+    if currentTime - startTime >= frameDelay:
+        framee+=1
+        startTime = currentTime
+    if framee==len(moveList):
+        framee=0
+        done=True
+        return framee,startTime,done
+    try:
+        screen.blit(moveList[frame],(x,y))
+    except:
+        pass
+    return framee,startTime,done
+
+def wasdInput(WFunction=None,AFunction=None,SFunction=None,DFunction=None):
+    """Simple WASD/arrow keys input listener. Also supports arrow keys. Args are functions to run when respective key is pressed"""
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]or keys[pygame.K_a]and AFunction is not None:
+        AFunction()
+    if keys[pygame.K_RIGHT]or keys[pygame.K_d]and DFunction is not None:
+        DFunction()
+    if keys[pygame.K_DOWN]or keys[pygame.K_s]and SFunction is not None:
+        SFunction()
+    if keys[pygame.K_UP]or keys[pygame.K_w]and WFunction is not None:
+        WFunction()
+def enableAll():
+    "Enables all created buttons"
+    for button in buttons:
+        button.enable()
+def disableAll():
+    "Disables all created buttons"
+    for button in buttons:
+        button.disable()
+def listenAll(screen):
+    "Listen on all active buttons at once"
+    global buttons
+    for button in buttons:
+        if button.active:
+            button.listen(screen)
+def showAll(screen):
+    "Show all active buttons at once"
+    for button in buttons:
+        if button.active:
+            button.show(screen)
+def listenPulseAll():
+    "Listen pulse on all created buttons at once"
+    global buttons
+    for button in buttons:
+        button.listenPulse()
 def save(saveFile:str,save):
     "Save a dictionary of variables to a json file"
     _save=save
     with open(saveFile,'w') as f:
         json.dump(_save,f)
-def load(saveFile:str)-> dict:
+def load(saveFile:str)-> dict|list:
     "Load a dictionary of variables from a json file and return it"
     _save=json.load(open(saveFile))
     return _save
@@ -181,7 +261,7 @@ def checkHover(x1:int,x2:int,y1:int,y2:int,function):
     mouseX,mouseY=pygame.mouse.get_pos()
     if mouseX>x1 and mouseX<x2 and mouseY>y1 and mouseY<y2:
         function()
-def loadMods(modDir:str,loadingScreen:pygame.Surface=None,screen:pygame.Surface=None):
+def loadMods(modDir:str,loadingScreen:pygame.Surface=None,screen:pygame.Surface=None)->list:
     "Load mods in given directory. Returns list of loaded mods as Module objects. The folder containing each mod's files must start with an uppercase letter and the .py file must be the same name but lowercase."
     loadedMods=[]
     logList=[]
@@ -225,15 +305,34 @@ def loadMods(modDir:str,loadingScreen:pygame.Surface=None,screen:pygame.Surface=
                     print(f'Mod named {name} successfully loaded.')
                     logList.append(f'{name}\'s mod object succesfully added to list of mods.')
                     logList.append(f'Mod named {name} successfully loaded.')
-                    if not loadingScreen is None:
+                    if loadingScreen is not None:
                         screen.blit(loadingScreen,(0,0))    
     log.update(log=logList)
     save('log.json',log)
     print('Log saved to log.json file.')
     return loadedMods
-def pleaseGodWork():
-    pass
+def renderUI():
+    for element in elements:
+        element.render()
+def autoWrap(text:str,width:int,font:pygame.font.Font,textColor):
+    "returns a list of surfaces of text autowrapped to a given width"
+    line=''
+    lines=[]
+    words=text.split(' ')
+    for i in range(len(words)):
+        if font.size(line+words[i]+' ')[0]<=width:
+            line+=words[i]+' '
+        else:
+            lines.append(font.render(line,True,textColor))
+            line=''
+            line+=words[i]+' '
+            i-=2
+    if line!='':
+        lines.append(font.render(line,True,textColor))
+    return lines
+#print(autoWrap('This is an incredible test i sure hope it doesn\'t go wrong',170,pygame.font.SysFont('comic sans ms',12),'white'))
+
 if __name__=='__main__':
     print('This script doesn\'t work on its own. Import it into a project to use the functions and classes defined here')
 else:
-    print('Using PyEngine v0.3 APLHA') 
+    print('Using PyEngine v0.4.1 APLHA') 
